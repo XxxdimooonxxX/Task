2ca9eeea336a486c25472bf571ec625858b3cb82
XxxdimooonxxX <63523101+XxxdimooonxxX@users.noreply.github.com>
2021-04-28 21:02:38 +0300

    Update GGG.php

--- GGG.php
+++ GGG.php
@@ -1,56 +1,63 @@
 <?php
+    // require_once 'router.php';
+    // $app = new Application();
+    // $app->run();
+    class Query{
+        // protected $tasks;
+        // protected $groups;
+        
+        // function __construct(){
+        //     $tasks = Array("name", "description", "type_task_id", "complite", "groups_id", "projects_id", "data_begin",
+        //         "data_end", "parent_id", "created_at", "created_by", "updated_at", "updated_by", "deleted_at", "deleted_by", "active", "lock");
+        //     $groups = ["name", "date_begin", "date_end", "description", "complite", "projects_id", "created_at",
+        //         "created_by", "updated_at","updated_by", "deleted_at","deleted_by", "active", "lock"];
+        // }
+        
+        public function insertTasks($arr = [], $name){
+            //$q = DB::create();
+            $arrS = [];//
+            $ins = "Insert into $name value (null, '";
 
-class Query{
-    public function insertTasks($arr = []){
-        //$q = DB::create();
-        //Таблица tsks
-        $arrName = ["name", "description", "type_task_id", "complite", "groups_id", "projects_id", "data_begin", "data_end", "parent_id", "created_at", "created_by", "updated_at", "updated_by", "deleted_at", "deleted_by", "active", "lock"];
-        $arrS = [];
-        $ins = "Insert into tasks value (null, '";
-        
-        
-        //перебирает названия атрибутов в массиве arrName
-        for($i = 0; $i < count($arrName); $i++){
-            //перебирает эллементы в ассоциативном массиве
-            foreach($arr as $key => $val){
-                //проверка, есть ли в ассоциативном массиве атрибут
-                if($arrName[$i] == $key){
-                    //если есть, то записываем его значение 
-                    $arrS[$arrName[$i]] = $val;
-                    break;
+            //массивы атрибутов
+            $tasks = Array("name", "description", "type_task_id", "complite", "groups_id", "projects_id", "data_begin",
+                "data_end", "parent_id", "created_at", "created_by", "updated_at", "updated_by", "deleted_at", "deleted_by", "active", "lock");
+            $groups = ["name", "date_begin", "date_end", "description", "complite", "projects_id", "created_at",
+                "created_by", "updated_at","updated_by", "deleted_at","deleted_by", "active", "lock"];
+            
+            //перебирает названия атрибутов в массиве конкретной таблицы
+            for($i = 0; $i < count($$name); $i++){
+                //перебирает эллементы в ассоциативном массиве
+                foreach($arr as $key => $val){
+                    //проверка, есть ли в ассоциативном массиве атрибут
+                    if($$name[$i] == $key){
+                        //если есть, то записываем его значение 
+                        $arrS[$$name[$i]] = $val;
+                        break;
+                    }else{
+                        //иначе присваеваем null. т.к его не указал 'пользователь'
+                        $arrS[$$name[$i]] = "null";
+                    }
+                }
+                //проверка, если этот элелемент последний, то добавляем к нему всё то, что нужно для sql(закрытие запроса)
+                if($i == count($$name) - 1){
+                    $ins .= $arrS[$$name[$i]]."');";
                 }else{
-                    //иначе присваеваем null. т.к его не указал 'пользователь'
-                    $arrS[$arrName[$i]] = "null";
+                    $ins .= $arrS[$$name[$i]]."', '";
                 }
             }
-            //проверка, если этот элелемент последний, то добавляем к нему всё то, что нужно для sql(закрытие запроса)
-            if($i == count($arrName) - 1){
-                $ins .= $arrS[$arrName[$i]]."');";
-            }else{
-                $ins .= $arrS[$arrName[$i]]."', '";
-            }
+            echo $ins;
+            //$q->qwery($ins)
         }
-        echo $ins;
-        //$q->qwery($ins)
+        
+        //таблица groups
+        //$arrName = ["name", "date_begin", "date_end", "description", "complite", "projects_id", "created_at", "created_by", "updated"]
     }
-    
-    
-    //таблица groups
-    $arrName = ["name", "date_begin", "date_end", "description", "complite", "projects_id", "created_at", "created_by", "updated"]
-    
-    
-    
-    
-    
-    
-}
-
-
-//
-$a = new Query();
-$a->insertTasks(Array(
-    'name' => 'Ivan',
-    'projects_id' => '2'
-));
-
+    //
+    $a = new Query();
+    $a->insertTasks(Array(
+        'name' => 'Ivan',
+        'projects_id' => '2'
+    ), "groups");
 ?>
+
+
